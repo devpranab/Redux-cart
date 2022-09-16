0cba3328264e62552ec1237fa9b0ad1a1b4d945a
pranav s <dev.pranabsk@gmail.com>
2022-09-16 14:44:09 +0530

    connect component with redux state and actions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Learn Redux
+1. setup a simple application to use redux
+2. set actions and dispatch for redux cart
+3. create Cart Reducers
+4. create store use react redux provider
+5. connect component with redux state and actions
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addToCart } from '../../redux/actions';
 import Product from '../Product/Product';
 
 const Shop = () => {
@@ -18,4 +20,20 @@ const Shop = () => {
     );
 };
 
-export default Shop;
+const mapStateToProps = state => {
+    return {
+        cart: state.cart,
+        products: state.products
+    }
+}
+
+const mapDispatchToProps = {
+    addToCart: addToCart
+}
+
+//const connectToStore = connect(mapStateToProps, mapDispatchToProps);
+//connectToStore(Shop);
+//connect(mapStateToProps, mapDispatchToProps) (Shop);
+//shortcut bellow
+
+export default connect(mapStateToProps, mapDispatchToProps) (Shop);
