2b46d09eef4327b26f1d6295d274a7f469c1bced
pranav s <dev.pranabsk@gmail.com>
2022-09-16 16:13:18 +0530

    use redux to read dynamic state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeFromCart } from '../../redux/actions';
 
-const Cart = () => {
+const Cart = (props) => {
+    console.log(props);
+    const {cart, removeFromCart} = props;
     return (
         <div>
             Cart Component
+            <ul>
+                {
+                    cart.map(id => <li>{id}<button onClick={() => removeFromCart(id)}>x</button></li>)
+                }
+            </ul>
         </div>
     );
 };
 
-export default Cart;
+const mapStateToProps = state => {
+    return {
+        cart: state.cart
+    }
+}
+
+const mapDispatchToProps = {
+    removeFromCart: removeFromCart
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Cart);
